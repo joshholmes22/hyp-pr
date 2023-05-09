4ed9ecf60ea77d65200441da38c19016eebef072
Josh Holmes <josh@glowingmanagement.com>
2023-05-09 08:59:46 +0100

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,85 @@
 import Image from "next/image";
+import { useState } from "react";
 
 function NavBar() {
-  return (
-    <div class="flex justify-between items-center p-5">
-      <div className="navbar__logo">
-        <Image
-          src="/images/hypprLogo.png"
-          alt="My logo"
-          width={200}
-          height={100}
-        />
+  const [showMenu, setShowMenu] = useState(false);
+
+  const NavLink = ({ children, href }) => {
+    return (
+      <div className="hover:underline hover:underline-offset-8 transition-all duration-500 ease-in-out">
+        <a href={href}>
+          <span className="text-lg">{children}</span>
+        </a>
       </div>
-      <div class="flex gap-7 text-xl">
-        <a href="#">SERVICES</a>
-        <a href="#">REVIEWS</a>
-        <a href="#">OUR WORK</a>
-        <a href="#">CONTACT US</a>
+    );
+  };
+
+  return (
+    <div className="flex justify-between items-center p-5 flex-col">
+      <div className="flex content-center">
+        <div className="cursor-pointer">
+          <Image
+            src="/images/hypprLogo.png"
+            alt="My logo"
+            width={200}
+            height={100}
+            className="hover:scale-125 transition duration-500 ease-in-out"
+          />
+        </div>
+
+        <div
+          className="md:hidden cursor-pointer hover:scale-110 transition duration-500 ease-in-out flex items-center"
+          onClick={() => setShowMenu(!showMenu)}
+          style={{ transform: showMenu ? "rotate(90deg)" : "" }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            {!showMenu ? (
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0
+                5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0
+                5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              />
+            ) : (
+              // This is the X icon
+              <path
+                fillRule="evenodd"
+                d="M10 11.414l4.95 4.95 1.414-1.414L11.414
+                    10l4.95-4.95-1.414-1.414L10 8.586 5.05 3.636
+                    3.636 5.05 8.586 10l-4.95 4.95 1.414 1.414L10
+                    11.414z"
+                clipRule="evenodd"
+              />
+            )}
+          </svg>
+        </div>
+      </div>
+
+      <div
+        className={`${
+          showMenu ? "flex" : "hidden"
+        } md:hidden flex-col gap-4 text-xl text-center pt-5`}
+      >
+        <NavLink href="#">HOME</NavLink>
+        <NavLink href="#">SERVICES</NavLink>
+        <NavLink href="#">REVIEWS</NavLink>
+        <NavLink href="#">OUR WORK</NavLink>
+        <NavLink href="#">CONTACT US</NavLink>
+      </div>
+
+      {/* This is the desktop version of the navbar */}
+      <div className="hidden md:flex gap-7 text-xl pt-5">
+        <NavLink href="#">HOME</NavLink>
+        <NavLink href="#">SERVICES</NavLink>
+        <NavLink href="#">REVIEWS</NavLink>
+        <NavLink href="#">OUR WORK</NavLink>
+        <NavLink href="#">CONTACT US</NavLink>
       </div>
     </div>
   );
