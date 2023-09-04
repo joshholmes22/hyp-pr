a7d1d2306756a9ffddfd5c8892cbb5363c16073f
Josh Holmes <josh@glowingmanagement.com>
2023-09-04 08:01:48 +0100

    update our work

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["d3g56pywmp84r0.cloudfront.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -3,163 +3,60 @@ import TempArtwork1 from "../../public/images/TempArtwork1.svg";
 import TempArtwork2 from "../../public/images/TempArtwork2.svg";
 import TempArtwork3 from "../../public/images/TempArtwork3.svg";
 import TempArtwork4 from "../../public/images/TempArtwork4.svg";
-import TempResults from "../../public/images/TempResults1.svg";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 
 function OurWork() {
-  const [emblaRef] = useEmblaCarousel(
-    { loop: true, containScroll: "trimSnaps" },
-    [Autoplay()]
-  );
-
-  const EmptySlide = () => <div className="embla_slide w-10 max-w-sm" />;
-
   const works = [
     {
       id: 1,
       artist: "Slab City",
-      artwork: TempArtwork1,
+      artwork: "https://d3g56pywmp84r0.cloudfront.net/video_Slab_City.mp4",
       objective: "Increase Spotify streams",
       strategy: "Social media ads",
       result: [
         "Added to 200 personal playlists",
         "Triggered algorithmic playlists",
       ],
-      image: TempResults,
+      image: "https://d3g56pywmp84r0.cloudfront.net/Our_Work_Slab_City.png",
     },
     {
       id: 2,
       artist: "Robbie Hutton",
-      artwork: TempArtwork2,
+      artwork: "https://d3g56pywmp84r0.cloudfront.net/video_Robbie_Hutton.mp4",
       objective: "Increase Spotify streams",
       strategy: "Social media ads",
       result: [
         "Added to 120 personal playlists",
         "Triggered algorithmic playlists",
       ],
-      image: TempResults,
+      image: "https://d3g56pywmp84r0.cloudfront.net/Our_Work_Robbie_Hutton.png",
     },
     {
       id: 3,
       artist: "Crooked Shepherd",
-      artwork: TempArtwork3,
+      artwork:
+        "https://d3g56pywmp84r0.cloudfront.net/video_Crooked_Shepherd.mp4",
       objective: "PR Campaign",
       strategy: "Spotify and Youtube Placements",
       result: [
         "Achieved over 40000 views on Youtube",
         "Achieved over 30000 streams on Spotify",
-        "Reviews in 4 music publications",
       ],
-      image: TempResults,
+      image:
+        "https://d3g56pywmp84r0.cloudfront.net/Our_Work_Crooked_Shepherd.png",
     },
     {
       id: 4,
       artist: "Neela",
-      artwork: TempArtwork4,
+      artwork: "https://d3g56pywmp84r0.cloudfront.net/video_Neela.mp4",
       objective: "Increase Spotify streams",
       strategy: "Social media ads",
       result: [
         "Triggered algorithmic playlists",
         "Added to Spotify Editorial playlist",
       ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
-    },
-    {
-      id: 4,
-      artist: "Neela",
-      artwork: TempArtwork4,
-      objective: "Increase Spotify streams",
-      strategy: "Social media ads",
-      result: [
-        "Triggered algorithmic playlists",
-        "Added to Spotify Editorial playlist",
-      ],
-      image: TempResults,
+      image: "https://d3g56pywmp84r0.cloudfront.net/Our_Work_Neela.png",
     },
   ];
 
@@ -172,43 +69,38 @@ function OurWork() {
     image,
   }) => {
     return (
-      <div
-        className={
-          "bg-custom-white text-custom-black text-center rounded-xl w-80 min-w-[21rem] max-w-sm shadow-lg"
-        }
-      >
-        <div className="flex justify-center">
-          <Image
-            src={artwork}
-            alt={`${artist} artwork`}
-            width={200}
-            height={100}
-            className="my-5"
-          />
+      <div className="bg-custom-white text-custom-black rounded-xl shadow-lg flex overflow-hidden m-4">
+        <div className="w-1/2">
+          <video width="100%" controls className="h-full">
+            <source src={artwork} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
-        <div className="text-left px-10">
-          <h3 className="font-bold text-xl pt-5">Artist: </h3>
-          <div className="pb-8">{artist}</div>
-          <h3 className="font-bold text-xl pt-5">Objective: </h3>
-          <div className="pb-8">{objective}</div>
-          <h3 className="font-bold text-xl pt-5">Strategy: </h3>
-          <div className="pb-8">{strategy}</div>
-          <h3 className="font-bold text-xl pt-5">Results:</h3>
-          <ul className="list-disc pl-5 pb-8">
-            {result.map((res, i) => (
-              <li key={i}>{res}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="flex justify-center">
-          <Image
-            src={image}
-            alt="Campaign Results"
-            width={200}
-            height={100}
-            className="my-5"
-          />
+        <div className="w-1/2 p-4 flex flex-col justify-between">
+          <div>
+            <h3 className="font-semibold text-2xl mb-2">{artist}</h3>
+            <h4 className="font-medium text-lg mb-1">Objective:</h4>
+            <p className="mb-2">{objective}</p>
+            <h4 className="font-medium text-lg mb-1">Strategy:</h4>
+            <p className="mb-2">{strategy}</p>
+            <h4 className="font-medium text-lg mb-1">Results:</h4>
+            <ul className="list-disc pl-5 mb-2">
+              {result.map((res, i) => (
+                <li key={i} className="mb-1">
+                  {res}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="mt-2">
+            <Image
+              src={image}
+              alt="Campaign Results"
+              layout="responsive"
+              width={200}
+              height={100}
+            />
+          </div>
         </div>
       </div>
     );
@@ -219,22 +111,19 @@ function OurWork() {
       <h2 className="text-5xl pt-3 text-custom-white text-center pb-5">
         Our Work
       </h2>
-      <section className="embla" ref={emblaRef}>
-        <div className="embla__container flex gap-10">
-          <EmptySlide />
-          {works.map((work) => (
-            <WorkCard
-              key={work.id}
-              artist={work.artist}
-              artwork={work.artwork}
-              objective={work.objective}
-              strategy={work.strategy}
-              result={work.result}
-              image={work.image}
-            />
-          ))}
-        </div>
-      </section>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
+        {works.map((work) => (
+          <WorkCard
+            key={work.id}
+            artist={work.artist}
+            artwork={work.artwork}
+            objective={work.objective}
+            strategy={work.strategy}
+            result={work.result}
+            image={work.image}
+          />
+        ))}
+      </div>
     </div>
   );
 }
