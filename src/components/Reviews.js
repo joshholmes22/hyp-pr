f63d6640b9c4e88fe901af897ae55fbd9fd07a48
Josh Holmes <josh@glowingmanagement.com>
2023-09-19 14:51:38 +0800

    update testimonials

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -5,27 +5,43 @@ function Reviews() {
 
   const reviews = [
     {
-      title: "Robbie Hutton",
-      video:
-        "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Robbie_Hutton.mp4",
-    },
-    {
       title: "Farhana",
+      quote:
+        "What stood out to me with Ryan, is he has an eagle eye for detail, a great knowledge of marketing with in the industry and how to build a fanbase",
       video: "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Farhana.mp4",
     },
     {
       title: "Crooked Shepherd",
+      quote:
+        "It was such an awesome experience working with you. It was super helpful and we're definitely going to recommend you to anyone else who needs these services",
       video:
         "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Crooked_Shepherd.mp4",
     },
     {
       title: "Emily Gabriele",
+      quote:
+        "I worked with Ryan for about a year and it was one of the best decisions that I ever made.",
       video:
         "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Emily_Gabriele.mp4",
     },
     {
       title: "Ivy League",
+      quote:
+        "I know I wouldn't be anywhere close to where I am now, if it wasn't for Ryan",
       video: "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Ivy_League.mp4",
+    },
+    {
+      title: "Aesthetic",
+      quote:
+        "Since I've been working with Ryan, my followers across social media have doubled, my Spotify streams have been skyrocketing and we break personal bests with every release.",
+      video: "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Aesthetic.mp4",
+    },
+    {
+      title: "Robbie Hutton",
+      quote:
+        "Working with Ryan our song got an 80% save to listener rate, was added to 120 personal playlists and gained me over 250 new followers on Instagram",
+      video:
+        "https://d3g56pywmp84r0.cloudfront.net/Testimonial_Robbie_Hutton.mp4",
     },
   ];
 
@@ -52,7 +68,9 @@ function Reviews() {
       className="text-center md:text-left bg-custom-white py-10"
       id="reviews"
     >
-      <h2 className="text-5xl pb-10 text-custom-black text-center">Reviews</h2>
+      <h2 className="text-5xl pb-10 text-custom-black text-center">
+        Testimonials
+      </h2>
       {reviews.slice(0, itemsToShow).map((review, index) => (
         <ReviewCard key={index} title={review.title} video={review.video} />
       ))}
